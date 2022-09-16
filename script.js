06fed5fe1ee606d97eb46a9e2a7f6552431f7e8d
TylerNeverlived <107303031+TylerNeverlived@users.noreply.github.com>
2022-09-16 17:42:31 +0300

    first refactor

--- script.js
+++ script.js
@@ -4,16 +4,22 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
+const displayGuessMessage = function (message) {
+  document.querySelector('.guess-message').textContent = message;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   const guessingNumber = Number(document.querySelector('.number-input').value);
 
   // No imput
   if (!guessingNumber) {
-    document.querySelector('.guess-message').textContent = 'Введите число!';
+    //  document.querySelector('.guess-message').textContent = 'Введите число!';
+    displayGuessMessage('Введите число!');
 
     // Player won
   } else if (guessingNumber === secretNumber) {
-    document.querySelector('.guess-message').textContent = 'Правыльно!';
+    //  document.querySelector('.guess-message').textContent = 'Правыльно!';
+    displayGuessMessage('Правыльно!');
     document.querySelector('.question').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = 'rgb(9, 250, 21)';
     document.querySelector('.question').style.width = '50rem';
@@ -23,35 +29,53 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.highscore').textContent = highScore;
     }
 
-    //Too high
-  } else if (guessingNumber > secretNumber) {
+    // Number from input is wrong
+  } else if (guessingNumber !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.guess-message').textContent = 'Слишком много!';
+      // document.querySelector('.guess-message').textContent =
+      //   guessingNumber > secretNumber ? 'Слишком много!' : 'Слишком мало!';
+      displayGuessMessage(
+        guessingNumber > secretNumber ? 'Слишком много!' : 'Слишком мало!'
+      );
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.guess-message').textContent = 'Game over!';
-      document.querySelector('.score').textContent = 0;
-    }
-
-    //Too low
-  } else if (guessingNumber < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.guess-message').textContent = 'Слишком мало!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.guess-message').textContent = 'Game over!';
+      // document.querySelector('.guess-message').textContent = 'Game over!';
+      displayGuessMessage('Game over!');
       document.querySelector('.score').textContent = 0;
     }
   }
+
+  //Too high
+  //   } else if (guessingNumber > secretNumber) {
+  //     if (score > 1) {
+  //       document.querySelector('.guess-message').textContent = 'Слишком много!';
+  //       score--;
+  //       document.querySelector('.score').textContent = score;
+  //     } else {
+  //       document.querySelector('.guess-message').textContent = 'Game over!';
+  //       document.querySelector('.score').textContent = 0;
+  //     }
+
+  //     //Too low
+  //   } else if (guessingNumber < secretNumber) {
+  //     if (score > 1) {
+  //       document.querySelector('.guess-message').textContent = 'Слишком мало!';
+  //       score--;
+  //       document.querySelector('.score').textContent = score;
+  //     } else {
+  //       document.querySelector('.guess-message').textContent = 'Game over!';
+  //       document.querySelector('.score').textContent = 0;
+  //     }
+  //   }
 });
 
 document.querySelector('.again').addEventListener('click', function () {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   score = 20;
 
-  document.querySelector('.guess-message').textContent = 'Начни угадывать!';
+  //   document.querySelector('.guess-message').textContent = 'Начни угадывать!';
+  displayGuessMessage('Начни угадывать!');
   document.querySelector('.score').textContent = score;
   document.querySelector('.question').textContent = '???';
   document.querySelector('.question').style.width = '25rem';
