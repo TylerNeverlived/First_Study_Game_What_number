85a1c24e7636408ae5196469f76d3362396ad15d
TylerNeverlived <107303031+TylerNeverlived@users.noreply.github.com>
2022-09-17 11:08:58 +0300

    second refactor

--- script.js
+++ script.js
@@ -8,6 +8,14 @@ const displayGuessMessage = function (message) {
   document.querySelector('.guess-message').textContent = message;
 };
 
+const displayQuestion = function (qmessage) {
+  document.querySelector('.question').textContent = qmessage;
+};
+
+const displayScore = function (smessage) {
+  document.querySelector('.score').textContent = smessage;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   const guessingNumber = Number(document.querySelector('.number-input').value);
 
@@ -20,7 +28,8 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guessingNumber === secretNumber) {
     //  document.querySelector('.guess-message').textContent = 'Правыльно!';
     displayGuessMessage('Правыльно!');
-    document.querySelector('.question').textContent = secretNumber;
+    //  document.querySelector('.question').textContent = secretNumber;
+    displayQuestion(secretNumber);
     document.querySelector('body').style.backgroundColor = 'rgb(9, 250, 21)';
     document.querySelector('.question').style.width = '50rem';
 
@@ -38,11 +47,13 @@ document.querySelector('.check').addEventListener('click', function () {
         guessingNumber > secretNumber ? 'Слишком много!' : 'Слишком мало!'
       );
       score--;
-      document.querySelector('.score').textContent = score;
+      // document.querySelector('.score').textContent = score;
+      displayScore(score);
     } else {
       // document.querySelector('.guess-message').textContent = 'Game over!';
       displayGuessMessage('Game over!');
-      document.querySelector('.score').textContent = 0;
+      // document.querySelector('.score').textContent = 0;
+      displayScore(0);
     }
   }
 
@@ -76,8 +87,10 @@ document.querySelector('.again').addEventListener('click', function () {
 
   //   document.querySelector('.guess-message').textContent = 'Начни угадывать!';
   displayGuessMessage('Начни угадывать!');
-  document.querySelector('.score').textContent = score;
-  document.querySelector('.question').textContent = '???';
+  //   document.querySelector('.score').textContent = score;
+  displayScore(score);
+  //   document.querySelector('.question').textContent = '??';
+  displayQuestion('??');
   document.querySelector('.question').style.width = '25rem';
   document.querySelector('body').style.backgroundColor = 'rgb(0, 0, 0)';
   document.querySelector('.number-input').value = '';
